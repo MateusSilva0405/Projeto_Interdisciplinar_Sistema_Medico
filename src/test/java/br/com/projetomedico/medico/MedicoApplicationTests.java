8d7249cdb557c2d5569aaa1513f297d8e898719a
Marcello Belchior <belchiormarcello@gmail.com>
2025-10-23 17:22:55 -0300

    Primeiro commit

---
+++ src/test/java/br/com/projetomedico/medico/MedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetomedico.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
